d59a3b6ee80e9735504f03c55c7c0ae5cdf78f3d
xchao <xhcao@microsoft.com>
2023-04-02 17:46:28 +0800

    markdown linkTarget to new tab

--- utils/TextFormatUtils.tsx
+++ utils/TextFormatUtils.tsx
@@ -2,7 +2,7 @@ import ReactMarkdown from 'react-markdown'
 import rehypeHighlight from 'rehype-highlight'
 
 export const formatBotMessage = (text: string) => 
-    <ReactMarkdown rehypePlugins={[rehypeHighlight]}>{text}</ReactMarkdown>
+    <ReactMarkdown linkTarget="_blank" rehypePlugins={[rehypeHighlight]}>{text}</ReactMarkdown>
   
 export const formatUserMessage = (text: string) => 
     text.split('\n').map((line, i) => (
